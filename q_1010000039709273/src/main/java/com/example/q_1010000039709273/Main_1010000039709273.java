974680183c551ede2a133e815486fcdf9c8bb7bf
zhouxin <429254164@qq.com>
2021-03-29 21:23:36 +0800

    修改一些注释

--- q_1010000039709273/src/main/java/com/example/q_1010000039709273/Main_1010000039709273.java
+++ q_1010000039709273/src/main/java/com/example/q_1010000039709273/Main_1010000039709273.java
@@ -3,9 +3,6 @@ package com.example.q_1010000039709273;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * 依赖查找示例
- * 1. 通过名称的方式来查找
- * 2.
  *
  * @author zhouxin
  * @since 2020/4/28 17:57
@@ -13,8 +10,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main_1010000039709273 {
 
     public static void main(String[] args) {
-        // 配置XML配置文件
-        // 启动Spring应用上下文
         ClassPathXmlApplicationContext beanFactory = new ClassPathXmlApplicationContext(
                 "classpath:/META-INF/dependency-lookup-context.xml");
 
